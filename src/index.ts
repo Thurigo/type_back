f4618faa3e84f2b00e3b9f2be0237f46ab99cfec
Thurigo <arthur.spier@gmail.com>
2023-10-02 16:21:42 -0300

    Recriado a classe do da classe que posso ser utilizada (Classe dentro do Game)

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,24 @@ class Jogador {
     }
 }
 
+class Classe extends Jogador {
+    classe: string
+    forca: number;
+    agilidade: number;
+    carisma: number;
+    defesa: number;
+    sorte: number;
 
+    constructor(nome: string, vida: number, gold: number, forca: number, agilidade: number, carisma: number, defesa: number, sorte: number, classe: string) {
+        super(nome, vida, gold);
+        this.forca = forca;
+        this.agilidade = agilidade;
+        this.carisma = carisma;
+        this.defesa = defesa;
+        this.sorte = sorte;
+        this.classe = classe;
+    }
+}
 
 
 let vida: number; // Alterei "real" para "number" já que não há tipo "real" no TypeScript
