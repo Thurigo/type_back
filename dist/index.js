421fc849695b31ec2213892b1de8ba72fe1ed8ee
Thurigo <arthur.spier@gmail.com>
2023-10-02 16:18:44 -0300

    Jogo do ano passado criado em visual g declaração de variaveis agora no Type

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log('Hello World');
+const number = 1;
+console.log(number);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log('Hello World');
+
+const number : number = 1;
+console.log(number);
+ 
