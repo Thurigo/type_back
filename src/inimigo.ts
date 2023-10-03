c4797aafdac1d6ffce0b5dd6b5a1d060273ef92b
Thurigo <arthur.spier@gmail.com>
2023-10-03 00:41:10 -0300

    classe inimigo criada

---
+++ src/inimigo.ts
@@ -0,0 +1,52 @@
+
+import {Randon_numero} from "./engine";
+
+class Inimigo {
+    
+    vida: number;
+    
+    nome: string;
+    forca: number;
+    agilidade: number;
+    carisma: number;
+    defesa: number;
+    sorte: number;
+    
+    
+    constructor(nome: string, vida: number, forca: number, agilidade: number, carisma: number, defesa: number, sorte: number) {
+        
+        
+        this.nome = nome;
+        this.vida = vida;
+        
+        this.forca = forca;
+        this.agilidade = agilidade;
+        this.carisma = carisma;
+        this.defesa = defesa;
+        this.sorte = sorte;
+        
+    }
+}
+
+function criarListaDeInimigo (numeroDeClasses: number): Inimigo[] {
+    const listaDeinimigo: Inimigo[] = [];
+    const nomes = ["Goblin", "Aranha", "Bruno", "Jose", "Cloti", "Edu", "Dragao", "Cleber"];
+
+    for (let i = 0; i < numeroDeClasses; i++) {
+        const nome = nomes[i % nomes.length];
+        const vida = Randon_numero(1, 20);
+        const forca = Randon_numero(1, 20);
+        const agilidade = Randon_numero(1, 20);
+        const carisma = Randon_numero(1, 20);
+        const defesa = Randon_numero(1, 20);
+        const sorte = Randon_numero(1, 20);
+
+        const inimigo = new Inimigo(nome, vida, forca, agilidade, carisma, defesa, sorte);
+        listaDeinimigo.push(inimigo);
+    }
+
+    return listaDeinimigo;
+}
+
+const listaDeClasses = criarListaDeInimigo(8);
+console.log(listaDeClasses);
