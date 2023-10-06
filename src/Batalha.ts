cf7604ebfa30f70050f96ab8c2c05139150776b1
Thurigo <arthur.spier@gmail.com>
2023-10-06 12:59:48 -0300

    Função de batalha contar com a sorte e já desenvolvido teste no jest

--- src/Batalha.ts
+++ src/Batalha.ts
@@ -1,7 +1,7 @@
 
 import { JogadorClasse, Guerreiro, Bardo, Mago, Ladrao } from './Playerclass';
 
-import { criarListaDeInimigo,Inimigo } from './inimigo'
+import { criarListaDeInimigo, Inimigo } from './inimigo'
 
 
 import { Randon_numero } from './randon_number';
@@ -15,45 +15,61 @@ export function DroparGold(sorte: number): number {
     return gold
 }
 
-export function Fugir(Jogador: JogadorClasse, inimigo1: Inimigo): JogadorClasse{
-    
+export function Fugir(Jogador: JogadorClasse, inimigo1: Inimigo): JogadorClasse {
+
     if (Jogador.classe && Jogador.classe.agilidade > inimigo1.agilidade) {
         return Jogador
     }
-    else if ((Jogador.classe.defesa + Jogador.vida) < inimigo1.forca){
-        Jogador.vida -= (inimigo1.forca -= Jogador.classe.defesa )
-    }else {
+    else if ((Jogador.classe.defesa + Jogador.vida) < inimigo1.forca) {
+        Jogador.vida -= (inimigo1.forca -= Jogador.classe.defesa)
+    } else {
         Jogador.classe.defesa -= inimigo1.forca
     }
 
     return Jogador
-} 
+}
 
 export function Atacar(Jogador: JogadorClasse, inimigo1: Inimigo): JogadorClasse {
 
     if (Jogador.classe && Jogador.classe.forca > inimigo1.defesa) {
         console.log('aranha morta');
         Jogador.gold += DroparGold(Jogador.classe.sorte);
-    }else if ((Jogador.classe.defesa + Jogador.vida) < inimigo1.forca){
-        Jogador.vida -= (inimigo1.forca -= Jogador.classe.defesa )
-    }else {
+    } else if ((Jogador.classe.defesa + Jogador.vida) < inimigo1.forca) {
+        Jogador.vida -= (inimigo1.forca -= Jogador.classe.defesa)
+    } else {
         Jogador.classe.defesa -= inimigo1.forca
     }
 
     return Jogador
-} 
+}
 
 
-export function Papear(Jogador: JogadorClasse, inimigo1: Inimigo): JogadorClasse{
-    
+export function Papear(Jogador: JogadorClasse, inimigo1: Inimigo): JogadorClasse {
+
     if (Jogador.classe && Jogador.classe.carisma > inimigo1.carisma) {
         return Jogador
     }
-    else if ((Jogador.classe.defesa + Jogador.vida) < inimigo1.forca){
-        Jogador.vida -= (inimigo1.forca -= Jogador.classe.defesa )
-    }else {
+    else if ((Jogador.classe.defesa + Jogador.vida) < inimigo1.forca) {
+        Jogador.vida -= (inimigo1.forca -= Jogador.classe.defesa)
+    } else {
         Jogador.classe.defesa -= inimigo1.forca
     }
 
     return Jogador
-} 
+}
+
+export function TentarSorte(Jogador: JogadorClasse, inimigo1: Inimigo): JogadorClasse {
+    const dado = Randon_numero(1, 2);
+    if (dado === 1) {
+        if ((Jogador.classe.defesa + Jogador.vida) < inimigo1.forca) {
+            Jogador.vida -= (inimigo1.forca -= Jogador.classe.defesa)
+        } else {
+            Jogador.classe.defesa -= inimigo1.forca
+        }
+    }else{
+        Jogador.classe.sorte += 1;
+    }
+
+
+    return Jogador
+}
