61fe43c9fc1571031bb97c43c53a26f2140bbceb
Thurigo <arthur.spier@gmail.com>
2023-10-06 02:07:04 -0300

    Função de batalha FUgir , criada e já realizado o test no jest

--- src/Batalha.ts
+++ src/Batalha.ts
@@ -1,7 +1,7 @@
 
 import { JogadorClasse, Guerreiro, Bardo, Mago, Ladrao } from './Playerclass';
 
-import { criarListaDeInimigo } from './inimigo'
+import { criarListaDeInimigo,Inimigo } from './inimigo'
 
 
 import { Randon_numero } from './randon_number';
@@ -14,14 +14,31 @@ export function DroparGold(sorte: number): number {
 
     return gold
 }
-
-export function Atacar(Jogador: JogadorClasse): JogadorClasse {
-
+export function Fugir(Jogador: JogadorClasse): JogadorClasse{
+    
     const dado = Randon_numero(1, 3);
     const listaDeinimigo = criarListaDeInimigo(3);
     const inimigo1 = listaDeinimigo[dado];
+    if (Jogador.classe && Jogador.classe.agilidade > inimigo1.agilidade) {
+        return Jogador
+    }
+    else if ((Jogador.classe.defesa + Jogador.vida) < inimigo1.forca){
+        Jogador.vida -= (inimigo1.forca -= Jogador.classe.defesa )
+    }else {
+        Jogador.classe.defesa -= inimigo1.forca
+    }
 
-    if (Jogador.classe.forca > inimigo1.defesa) {
+    return Jogador
+} 
+
+export function Atacar(Jogador: JogadorClasse): JogadorClasse {
+
+    const dado = Randon_numero(1, 2);
+    const listaDeinimigo = criarListaDeInimigo(3);
+    const inimigo1 = listaDeinimigo[dado];
+    console.log(inimigo1);
+
+    if (Jogador.classe && Jogador.classe.forca > inimigo1.defesa) {
         console.log('aranha morta');
         Jogador.gold += DroparGold(Jogador.classe.sorte);
     }else if ((Jogador.classe.defesa + Jogador.vida) < inimigo1.forca){
@@ -32,3 +49,4 @@ export function Atacar(Jogador: JogadorClasse): JogadorClasse {
 
     return Jogador
 } 
+
